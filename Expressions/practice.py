ba11d5e2f1d89258758068656faab0fe1e2ed647
Valerie5801 <valerie.yang@ucas-edu.net>
2024-09-10 20:18:00 +0000

    assigned 9 equations to 9 variables and printed them all out

---
+++ Expressions/practice.py
@@ -0,0 +1,19 @@
+equationOne = 7-24/8*4+6;
+equationTwo = 18/3-7+2*5;
+equationThree = 6*4/12+72/8-9;
+equationFour = (17-6/2)+4*3;
+equationFive = -2*(1*4-2/2)+(6+2-3);
+equationSix = -1*((3-4*7)/5)-2*24/6;
+equationSeven = (3*5**2/15)-(5-2**2);
+equationEight = (1**4*2**2+3**3)-2**5/4;
+equationNine = (22/2-2*5)**2+(4-6/6)**2;
+
+print(equationOne);
+print(equationTwo);
+print(equationThree);
+print(equationFour);
+print(equationFive);
+print(equationSix);
+print(equationSeven);
+print(equationEight);
+print(equationNine);
