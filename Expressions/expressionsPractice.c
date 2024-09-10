16109ba2cae182a81c8045e251616ff3ea6c2c5b
Valerie5801 <valerie.yang@ucas-edu.net>
2024-09-10 23:04:05 +0000

    tried to add exponents not finished yet

--- Expressions/expressionsPractice.c
+++ Expressions/expressionsPractice.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void){
     //equationOne 
-    printf("/n %d",7-24/8*4+6);
+    printf("\n %d",7-24/8*4+6);
     //equationTwo 
-    printf("/n %d", 18/3-7+2*5);
+    printf("\n %d", 18/3-7+2*5);
     //equationThree 
-    printf("/n %d",6*4/12+72/8-9);
+    printf("\n %d",6*4/12+72/8-9);
     //equationFour 
-    printf("/n %d", (17-6/2)+4*3);
+    printf("\n %d", (17-6/2)+4*3);
     //equationFive 
-    printf("/n %d",-2*(1*4-2/2)+(6+2-3));
+    printf("\n %d",-2*(1*4-2/2)+(6+2-3));
     //equationSix 
-    printf("/n %d",-1*((3-4*7)/5)-2*24/6);
+    printf("\n %d",-1*((3-4*7)/5)-2*24/6);
     //equationSeven 
-    printf("/n %d", (3*5));
+    printf("\n %f", (3*pow(5, 2)/15)-(5-pow(2, 2)));
     //equationEight 
-    printf("/n %d",7-24/8*4+6);
+    printf("\n %d", (pow(1, 4)*pow(2, 2)+pow(3, 3))-pow(2, 5)/4);
     //equationNine 
-    printf("/n %d",7-24/8*4+6);
+    printf("\n %d",7-24/8*4+6);
     return 0;
 }
