82b4b1f633be8a390fd376ded60f1cd62c9a6823
Valerie5801 <valerie.yang@ucas-edu.net>
2024-09-10 20:42:16 +0000

    printing equations

--- Expressions/expressionsPractice.c
+++ Expressions/expressionsPractice.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Hello World");
+    //equationOne 
+    printf("/n %d",7-24/8*4+6);
+    //equationTwo 
+    printf("/n %d", 18/3-7+2*5);
+    //equationThree 
+    printf("/n %d",6*4/12+72/8-9);
+    //equationFour 
+    printf("/n %d", (17-6/2)+4*3);
+    //equationFive 
+    printf("/n %d",-2*(1*4-2/2)+(6+2-3));
+    //equationSix 
+    printf("/n %d",-1*((3-4*7)/5)-2*24/6);
+    //equationSeven 
+    printf("/n %d", (3*5));
+    //equationEight 
+    printf("/n %d",7-24/8*4+6);
+    //equationNine 
+    printf("/n %d",7-24/8*4+6);
     return 0;
 }
