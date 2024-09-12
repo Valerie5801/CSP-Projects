af54886c2c542a3fadbd03e92612beb142f044da
Valerie5801 <valerie.yang@ucas-edu.net>
2024-09-12 23:40:44 +0000

    adding comments to organize the code better

--- Expressions/finance-calc.py
+++ Expressions/finance-calc.py
@@ -1,9 +1,12 @@
 print("This calculator will calculate your budget for the month.")
+#getting user inputs
 income = float(input("What is your income?: "))
 rent = float(input("What is your monthly rent?: "))
 utilites = float(input("What is your utility cost?: "))
 groceries = float(input("What is your groceries cost?: "))
 transportation = float(input("What is your transportation cost?: "))
+
+#calculations
 expenses = rent + utilites + groceries + transportation;
 savings = income * 0.2;
 total = income - expenses - savings;
@@ -14,6 +17,7 @@ pGroceries = groceries/income * 100;
 pTransportation = transportation/income * 100;
 pExpenses = expenses/income * 100;
 
+#prints
 print("Your income is: $", income);
 print("Your expenses are: $", expenses);
 print("Your savings are: $", savings);
