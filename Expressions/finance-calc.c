d875dd3c138703c8065aa561bde892aa4d2a116e
Valerie5801 <valerie.yang@ucas-edu.net>
2024-09-13 04:14:13 +0000

    fixed the bug and finished the code

--- Expressions/finance-calc.c
+++ Expressions/finance-calc.c
@@ -2,21 +2,40 @@
 
 int main(void){
     float income, rent, utilites, groceries, transportation, expenses, savings, total;
-    printf("This is going to calculate your budget for this month.");
+    float pRent, pUtilites, pGroceries, pTransportation, pExpenses;
+    printf("This is going to calculate your budget for this month.\n");
     //income
-    printf("How much do you make a month?: \n");
-    scanf("%f", income);
+    printf("How much do you make a month?:\n");
+    scanf("%f", &income);
     //rent
-    printf("What is your rent?: \n");
-    scanf("%f", rent);
+    printf("How much is your rent?:\n");
+    scanf("%f", &rent);
     //utilites
-    printf("What is your utility price?: \n");
-    scanf("%f", utilites);
+    printf("How much do you spend on utilites?:\n");
+    scanf("%f", &utilites);
     //groceries
-    printf("How much do you spend on groceries?: \n");
-    scanf("%f", groceries);
+    printf("How much do you spend on groceries?:\n");
+    scanf("%f", &groceries);
     //transportation
-    printf("How much do you spend on transportation?: \n");
-    scanf("%f", transportation);
+    printf("How much do spend on transportation?:\n");
+    scanf("%f", &transportation);
+    //calculations
+    expenses = rent + utilites + groceries + transportation;
+    savings = income*.2;
+    total = income - expenses - savings;
+    pRent = rent/income; 
+    pUtilites = utilites/income; 
+    pGroceries = groceries/income; 
+    pTransportation = transportation/income; 
+    pExpenses = expenses/income;
+
+    printf("Your income is: $ %2.f \n", income);
+    printf("Your expenses are: $ %.2f \n", expenses);
+    printf("Your savings are: $ %.2f \n", savings);
+    printf("Your total left to spend is: $ %.2f", total);
+    printf("\n Your rent cost is %.2f", pRent, "percent of  your income");
+    printf("\n Your utility cost is %.2f", pUtilites, "percent of your income");
+    printf("\n Your grocery cost is %.2f", pGroceries, "percent of your income");
+    printf("\n Your transportation cost is %.2f", pTransportation, "percent of your income");
     return 0;
 }
