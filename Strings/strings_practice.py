fedb94f67147f9b62d9d1c1b94e0acf5eb837f0e
Valerie5801 <valerie.yang@ucas-edu.net>
2024-09-18 20:19:03 +0000

    made a name decorater in python

---
+++ Strings/strings_practice.py
@@ -0,0 +1,13 @@
+#get user name
+print("This will decorate your name.")
+name = input("Please tell me your name: ")
+
+#decorate user name
+print("<<<", name, ">>>")
+print("(((", name, ")))")
+print("---", name, "---")
+print(":) :)", name, ":) :)")
+print("###", name, "###")
+print("+++", name, "+++")
+print("~~~", name, "~~~")
+print("===", name, "===")
