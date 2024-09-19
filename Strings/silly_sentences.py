f9fa4649845a53a60fc731713d4d625b9645e6f7
Valerie5801 <valerie.yang@ucas-edu.net>
2024-09-19 22:25:36 +0000

    made a silly sentences thing in python

---
+++ Strings/silly_sentences.py
@@ -0,0 +1,6 @@
+name = input("Type a name: ")
+place = input("Type a place: ")
+verb = input("Type a verb: ")
+noun = input("Type a noun: ")
+
+print(name+" went to the "+place+" where they "+verb+" and bought a "+noun+".")
