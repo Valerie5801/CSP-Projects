3946456e75f84f20c6e65928e7e29a9f4610a4e5
Valerie5801 <valerie.yang@ucas-edu.net>
2024-09-19 22:22:56 +0000

    revised strings and created a new file for silly-sentences in python

--- Strings/strings_practice.c
+++ Strings/strings_practice.c
@@ -4,10 +4,10 @@
 int main(void){
     //assign variable and get name.
     char name[30];
-    char decorName[] = ":) :)";
-    char decorNameTwo[] = ":) :)";
+    char decorName[50] = "<<<";
+    char decorNameTwo[] = ">>>";
     printf("This will decorate your name. \n");
-    printf("Please tell me your name: \n");
+    printf("Please tell me your name: ");
     scanf("%s", name);
     //decorate name.
     strcat(decorName, name);
