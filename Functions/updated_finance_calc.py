d9da6d4a98dd0bbb68707222348b60650e867ed9
Valerie5801 <valerie.yang@ucas-edu.net>
2024-09-27 02:06:22 +0000

    fixed teh updated finance calc

--- Functions/updated_finance_calc.py
+++ Functions/updated_finance_calc.py
@@ -2,32 +2,32 @@ print("This calculator will calculate your budget for the month.")
 #getting user inputs
 
 def iput(type):
-    var = float(input(f"How much is your {type}?: "))
+    var = input(f"How much is your {type}?: ")
     #print(f"Your {type} is $.")
     return f"{var}"
 
-income = iput("income")
-rent = iput("rent")
-utilites = iput("utilites")
-groceries = iput("groceries")
-transportation = iput("transportation")
+income = float(iput("income"))
+rent = float(iput("rent"))
+utilites = float(iput("utilites"))
+groceries = float(iput("groceries"))
+transportation = float(iput("transportation"))
 
 #calculations
 expenses = rent + utilites + groceries + transportation;
 print(expenses)
-#savings = income * 0.2;
-#total = income - expenses - savings;
+savings = income * 0.2;
+total = income - expenses - savings;
 
 def percent(type, amount):
     per = amount/income*100
     print(f"Your {type} is {per}% of your income.")
 
 #prints
-#print(income)
-#print(expenses)
-#print(savings)
-#print(total)
-#percent("rent", rent)
-#percent("utilites", utilites)
-#percent("groceries", groceries)
-#percent("transportation", transportation)
+print("Your income is $", income)
+print("Your utilites is $", utilites)
+print("Your groceries is $", groceries)
+print("Your transportation is $", transportation)
+percent("rent", rent)
+percent("utilites", utilites)
+percent("groceries", groceries)
+percent("transportation", transportation)
