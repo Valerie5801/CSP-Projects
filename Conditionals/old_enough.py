3817b01a54775a181ce283d35cf2304a22ec692f
Valerie5801 <valerie.yang@ucas-edu.net>
2024-09-30 20:30:37 +0000

    made the thing so it tells what the person can do at their age in python

---
+++ Conditionals/old_enough.py
@@ -0,0 +1,12 @@
+age = 15
+
+if age >= 18:
+    print("You can vote.")
+elif age >= 16:
+    print("You can drive.")
+elif age >= 15:
+    print("You can get a learner's permit.")
+elif age >= 5:
+    print("You can go to school.")
+else:
+    print("You can't go to school.")
