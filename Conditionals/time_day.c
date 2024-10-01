e8820b4856abdee71ed6013e896f9554beb9d1f7
Valerie5801 <valerie.yang@ucas-edu.net>
2024-10-01 20:09:46 +0000

    made a time day in c

---
+++ Conditionals/time_day.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(void){
+    int time = 400;
+    if (time >= 500 && time < 1100){
+        printf("Good morning");
+    }else if (time >= 1300 && time < 1700){
+        printf("Good afternoon");
+    }else if (time >= 1700 && time < 2200){
+        printf("Good evening");
+    }else{
+        printf("Good night");
+    }
+
+    return 0;
+}
