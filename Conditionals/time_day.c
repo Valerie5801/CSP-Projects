72a3721a608cd729ab2b68d51551903b8bb4b56d
Valerie5801 <valerie.yang@ucas-edu.net>
2024-10-02 19:46:51 +0000

    reset code

--- Conditionals/time_day.c
+++ Conditionals/time_day.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
-#include <time.h>
 
 int main(void){
-    int time = 1700;
-    if (time >= 500 && time < 1100){
-        printf("Good morning");
-    }else if (time >= 1300 && time < 1700){
-        printf("Good afternoon");
-    }else if (time >= 1700 && time < 2200){
-        printf("Good evening");
-    }else{
-        printf("Good night");
-    }
-
+    printf("hi world");
     return 0;
 }
