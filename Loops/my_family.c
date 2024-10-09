9c3774f3e05727084316a0b004f9db2c99e72c5f
Valerie5801 <valerie.yang@ucas-edu.net>
2024-10-09 20:27:07 +0000

    my_family loops in c

--- Loops/my_family.c
+++ Loops/my_family.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 char members[3][20] = {"Ellyana", "Vilas", "Valerie"};
-int i = 0;
 
 int main(void){
+    int i;
     for(i=0;i<3;i++){
-        printf("Hi\n %s ",);
+        printf("Hi %s\n", members[i]);
     }
     return 0;
 }
