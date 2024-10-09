4fd9b569f1a721e5faa426fb44f1b5cbd7d9e7a9
Valerie5801 <valerie.yang@ucas-edu.net>
2024-10-09 20:08:45 +0000

    failed to make a family loop in c but succeeded in python

---
+++ Loops/my_family.c
@@ -0,0 +1,5 @@
+char members[3][20] = {"Ellyana", "Vilas", "Valerie"};
+
+while(i<3){
+    printf("Hello")
+}
---
+++ Loops/my_family.py
@@ -0,0 +1,9 @@
+members = ["Ellyana", "Vilas", "Valerie"]
+i = 0
+
+for mem in members:
+    if mem == "Valerie":
+        print(f"Oh that's me, {mem}")
+    else:
+        print(f"Hi, {mem}")
+    i +=1
