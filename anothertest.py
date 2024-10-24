1f7ee1d0af0466d094fcb41112adeb58cefb6408
Valerie5801 <valerie.yang@ucas-edu.net>
2024-10-24 20:18:50 +0000

    made some tests fo rfinal

---
+++ anothertest.py
@@ -0,0 +1,5 @@
+keyCount = 3
+if keyCount==4:
+    print("You use the keys on their corresponding padlocks. Then you open the door to the room. \nYou finally exit the building, and see a city in the distance. Time to go home.")
+else:
+    print("Looks like you need more keys.\n")
--- testfinal.py
+++ testfinal.py
@@ -1,3 +1,5 @@
+keyCount =0 
+
 def selRm(void):
     puzzleSel = input("Select a wall(1,2,3,4,End): ")
     if puzzleSel == "End":
@@ -5,19 +7,21 @@ def selRm(void):
     else:
         return f"You walk over to wall {puzzleSel}."
 
-keyCount = 0
 print("You look around the room and see a letter on the ground. There is also a keypad with letters on the nearby wall.")
 inspect = input("Do you want to inspect further?:")
 if inspect=="yes":
     print("The letter says: dear Leo. Occasionally, Leah kind of annoys the dog, Luna. \nYou should've fed the Goat by the way. At least you didn't Give the 'thing' away.")
-    solution = "Lollygag"
-    userThinkSol = input("What is the secret word?:")
-    if userThinkSol == solution:
-        print("You type in your word on the panel on the wall. Aha! The panel flips over, and on the other side is a key.\n")
-        keyCount +=  1
-    elif userThinkSol == "lollygag":
-        print("You think that's the right word, but something is off.\n")
-    else:
-        print("You type in your word on the panel on the wall. Oh. Nothing happens")
+    while True:
+        solution = "Lollygag"
+        userThinkSol = input("What is the secret word?:")
+        if userThinkSol == solution:
+            print("You type in your word on the panel on the wall. Aha! The panel flips over, and on the other side is a key.\n")
+            keyCount +=  1
+            break
+        elif userThinkSol == "lollygag":
+            print("You think that's the right word, but something is off.\n")
+            break
+        else:
+            print("You type in your word on the panel on the wall. Oh. Nothing happens\n")
 else:
     print(selRm("ef"))
