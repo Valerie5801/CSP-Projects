ad28bafa2459c84aaa43ca3e4d50f1078f68df4c
Valerie5801 <valerie.yang@ucas-edu.net>
2024-10-24 20:35:12 +0000

    deleted

--- anothertest.py
+++
@@ -1,5 +0,0 @@
-keyCount = 3
-if keyCount==4:
-    print("You use the keys on their corresponding padlocks. Then you open the door to the room. \nYou finally exit the building, and see a city in the distance. Time to go home.")
-else:
-    print("Looks like you need more keys.\n")
